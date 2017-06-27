e15a6a6c2e1a53e71283fe5871f7863722e85628
matthew gray <mgray@rodeofx.com>
2017-06-26 21:32:06 -0700

    Add current roles, jobs, and events to edit project page. THIS IS TEMPORARY UNTIL I FINALIZE THE LAYOUT OF THE ROLES LIST. Once the layout is final, I can make the small changes to each one, but for now, this serves the purpose well enough

--- AgencyApp/python/post_project.py
+++ AgencyApp/python/post_project.py
@@ -9,6 +9,9 @@ class ProjectPostInstance(post.GenericPostInstance):
         kwargs["postType"] = constants.PROJECT_POST
         super(ProjectPostInstance, self).__init__(*args, **kwargs)
         self._database = models.ProjectPost
+        self._roles = None
+        self._jobs = None
+        self._events = None
 
     def checkModelFormValues(self):
         """TODO, only thing so far is status, which is optional, so return True"""
@@ -19,78 +22,6 @@ class ProjectPostInstance(post.GenericPostInstance):
 
     def saveModelFormValues(self):
         return True
-
-
-class CreateProjectPostView(post.GenericCreatePostView):
-    def __init__(self, *args, **kwargs):
-        kwargs["postType"] = constants.PROJECT_POST
-        super(CreateProjectPostView, self).__init__(*args, **kwargs)
-
-    @property
-    def projectID(self):
-        if self._projectID is None:
-            self._projectID = self.request.POST.get("projectID", helpers.getMessageFromKey(self.request, "projectID"))
-            if not self._projectID:
-                self._projectID = helpers.createUniqueID(destDatabase=models.ProjectPost,
-                                                        idKey="postID")
-        return self._projectID
-
-    @property
-    def postID(self):
-        if self._postID is None:
-            self._postID = self.projectID
-        return self._postID
-
-    @property
-    def cancelSource(self):
-        if self._cancelSource is None:
-            self._cancelSource = self.currentPage
-        return self._cancelSource 
-
-    @property
-    def cancelButtonExtraInputs(self):
-        if not self._cancelButtonExtraInputs:
-            self._cancelButtonExtraInputs = super(CreateProjectPostView, self).cancelButtonExtraInputs or {}
-        self._cancelButtonExtraInputs["projectID"] = self.postID
-        return self._cancelButtonExtraInputs
-
-    def cancelPage(self):
-        pass
-
-    @property
-    def post(self):
-        if self._post is None:
-            self._post = ProjectPostInstance(request=self.request, postID=self.postID, postType=constants.PROJECT_POST, formSubmitted=self.formSubmitted)
-        return self._post
-
-    @property
-    def formInitialValues(self):
-        self._formInitialValues = super(CreateProjectPostView, self).formInitialValues
-        if self.post.record:
-            self._formInitialValues["status"] = self.post.record.status
-        return self._formInitialValues
-
-    @property
-    def pageContext(self):
-        self._pageContext = super(CreateProjectPostView, self).pageContext
-        self._pageContext["statusOptions"] = constants.PROJECT_STATUS_LIST
-        self._pageContext["defaultStatus"] = "In production"
-        return self._pageContext
-
-class ViewProjectPostView(post.GenericViewPostView):
-    def __init__(self, *args, **kwargs):
-        super(ViewProjectPostView, self).__init__(*args, **kwargs)
-        self._roles = None
-        self._jobs = None
-        self._events = None
-
-    @property
-    def post(self):
-        if self._post is None:
-            if self.postID:
-                print "creating post"
-                self._post = ProjectPostInstance(request=self.request, postID=self.postID, postType=constants.PROJECT_POST)
-        return self._post
 
     @property
     def roles(self):
@@ -147,6 +78,81 @@ class ViewProjectPostView(post.GenericViewPostView):
                     self._events.append(newEvent)
         return self._events
 
+
+class CreateProjectPostView(post.GenericCreatePostView):
+    def __init__(self, *args, **kwargs):
+        kwargs["postType"] = constants.PROJECT_POST
+        super(CreateProjectPostView, self).__init__(*args, **kwargs)
+
+    @property
+    def projectID(self):
+        if self._projectID is None:
+            self._projectID = self.request.POST.get("projectID", helpers.getMessageFromKey(self.request, "projectID"))
+            if not self._projectID:
+                self._projectID = helpers.createUniqueID(destDatabase=models.ProjectPost,
+                                                        idKey="postID")
+        return self._projectID
+
+    @property
+    def postID(self):
+        if self._postID is None:
+            self._postID = self.projectID
+        return self._postID
+
+    @property
+    def cancelSource(self):
+        if self._cancelSource is None:
+            self._cancelSource = self.currentPage
+        return self._cancelSource 
+
+    @property
+    def cancelButtonExtraInputs(self):
+        if not self._cancelButtonExtraInputs:
+            self._cancelButtonExtraInputs = super(CreateProjectPostView, self).cancelButtonExtraInputs or {}
+        self._cancelButtonExtraInputs["projectID"] = self.postID
+        return self._cancelButtonExtraInputs
+
+    def cancelPage(self):
+        pass
+
+    @property
+    def post(self):
+        if self._post is None:
+            self._post = ProjectPostInstance(request=self.request, postID=self.postID, postType=constants.PROJECT_POST,
+                                             formSubmitted=self.formSubmitted)
+        return self._post
+
+    @property
+    def formInitialValues(self):
+        self._formInitialValues = super(CreateProjectPostView, self).formInitialValues
+        if self.post.record:
+            self._formInitialValues["status"] = self.post.record.status
+        return self._formInitialValues
+
+    @property
+    def pageContext(self):
+        self._pageContext = super(CreateProjectPostView, self).pageContext
+        self._pageContext["statusOptions"] = constants.PROJECT_STATUS_LIST
+        self._pageContext["defaultStatus"] = "In production"
+        self._pageContext["roles"] = self.post.roles
+        self._pageContext["jobs"] = self.post.jobs
+        self._pageContext["events"] = self.post.events
+        return self._pageContext
+
+class ViewProjectPostView(post.GenericViewPostView):
+    def __init__(self, *args, **kwargs):
+        super(ViewProjectPostView, self).__init__(*args, **kwargs)
+        self._roles = None
+        self._jobs = None
+        self._events = None
+
+    @property
+    def post(self):
+        if self._post is None:
+            if self.postID:
+                self._post = ProjectPostInstance(request=self.request, postID=self.postID, postType=constants.PROJECT_POST)
+        return self._post
+
     @property
     def pageContext(self):
         self._pageContext = super(ViewProjectPostView, self).pageContext
@@ -158,9 +164,9 @@ class ViewProjectPostView(post.GenericViewPostView):
                                                      }
         self._pageContext["project"] = self.post
         self._pageContext["projectID"] = self.post.record.postID
-        self._pageContext["roles"] = self.roles
-        self._pageContext["jobs"] = self.jobs
-        self._pageContext["events"] = self.events
+        self._pageContext["roles"] = self.post.roles
+        self._pageContext["jobs"] = self.post.jobs
+        self._pageContext["events"] = self.post.events
         return self._pageContext
 
 
