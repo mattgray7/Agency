310cb837f03cff67e07188898a8c525486a9bedf
matthew gray <mgray@rodeofx.com>
2017-07-05 14:26:22 -0700

    First pass at designing section headers

--- AgencyApp/static/AgencyApp/js/post/post.js
+++ AgencyApp/static/AgencyApp/js/post/post.js
@@ -1,4 +1,4 @@
-function addCreateCastingPost(formDict, formURL, formName, projectInfo){
+function addCreateCastingPost(formDict, formURL, formName){
 	var formString = "<form method='post' action='" + formURL + "' id='" + formName + "' class='form-style-1' style='width: 90%; background: none; margin-left: -10px;' enctype='multipart/form-data'>";
 	formString += "<table style='width: 100%;'><tr>"
 
@@ -7,12 +7,12 @@ function addCreateCastingPost(formDict, formURL, formName, projectInfo){
 	var pictureColumn = '<td style="max-width: 200px; height: 200px; text-align: center;"><div id="postPicturePanel" class="postPicture" style="width: 80%; height: 93%; background: #000; margin: 0 auto;"><img id="postPictureImg" src="' + pictureField.value + '" style="max-width:100%; max-height:100%;"/></div><div style="width: 60%; margin: 0 auto; overflow: hidden;">' + pictureField.input + "</div></td>";
 
 	// Fill text content
-	var sectionMap = {"Details": ["title", "characterType", "status"],
+	var sectionMap = {"Details": ["title", "project", "characterType", "status"],
 					  "Production": ["paid", "hoursPerWeek", "startDate", "endDate"],
 					  "The Role": ["characterName", "shortCharacterDescription", "description", "skills", "languages"],  
 					  "Physical": ["hairColor", "eyeColor", "complexion", "height", "build", "gender", "ageRange"]}
-	var mainLabelsColumn = "<td class='editPostLabelPanel' style='width: 20%; height: 280px; position:relative;'><div style='position: absolute; bottom:0; right: 0; margin-right: 5px;'><ul style='margin-bottom: -14px;'>";
-	var mainInputsColumn = "<td class='editPostInputPanel' style='width: 40%; height: 280px; position: relative;'><ul style='position: absolute; bottom: 0; width: 97%; margin-bottom: -8px;'><h1 style='font-size: 2.8em; padding: 0em 0em 0.3em 0em;'>Edit Role</h1>";
+	var mainLabelsColumn = "<td class='editPostLabelPanel' style='width: 20%; position:relative;'><div style=''><ul style='margin-bottom: -14px;'>";
+	var mainInputsColumn = "<td class='editPostInputPanel' style='width: 40%; position: relative;'><ul style=''><h1 style='font-size: 2.8em; padding: 0em 0em 0.3em 0em;'>Edit Role</h1>";
 	var otherLabelsColumn = "<td class='editPostLabelPanel' style='width: 25%; height: 600px; position:relative;'><div style='position: absolute; top:0; right: 0; margin-right: 5px; width: 90%; margin-top: 5px;'><ul>";
 	var otherInputsColumn = "<td class='editPostInputPanel' colspan='2' style='width: 80%;height: 600px;'><div style='margin-top: 12px;'><ul>";
 	for(sectionTitle in sectionMap){
@@ -26,10 +26,24 @@ function addCreateCastingPost(formDict, formURL, formName, projectInfo){
 			sectionLabelTableElement = otherLabelsColumn;
 			sectionInputTableElement = otherInputsColumn;
 		}
-
+		sectionLabelTableElement += "<br><div style='height: 20px'></div>"
+		sectionInputTableElement += "<div style='position: relative; height: 50px; margin-bottom: 0px;'><h2 class='editPostSectionTitle' style='position: absolute; z-index: 1;'> " + sectionTitle + "</h2><div style='position: absolute; z-index: 0; width: 145%; border: 1px solid #7c7b7b; height: 0px; bottom: 0; margin-bottom: 15px; margin-left: -45%;'></div></div>"
 		for(i in fieldList){
 			var fieldName = sectionMap[sectionTitle][i];
 			var field = formDict[fieldName];
+
+			// Add project
+			if(fieldName === "project"){
+				sectionLabelTableElement += "<label for='name'>Project</label><br>";
+				sectionInputTableElement += "<li style='margin-bottom: -5px;'><a  onclick='redirectToPost(" + '"' + field.projectID + '"' + ");'>"
+				if(field.title != null){
+					sectionInputTableElement += field.title;
+				}else{
+					sectionInputTableElement += "Not specified";
+				}
+				sectionInputTableElement += "</a></li>";
+				continue;
+			}
 			if(!field.hidden){
 				if(field.numRows > 1){
 					// stupid hack I hate myself right now
@@ -62,7 +76,7 @@ function addCreateCastingPost(formDict, formURL, formName, projectInfo){
 
 	}
 	
-	// Add project link
+	/*// Add project link
 	var projectTitle = null;
 	var projectID = null;
 	if(projectInfo != null){
@@ -75,10 +89,10 @@ function addCreateCastingPost(formDict, formURL, formName, projectInfo){
 	}else{
 		mainInputsColumn += "Not specified";
 	}
-	mainInputsColumn += "</a></li>";
+	mainInputsColumn += "</a></li>";*/
 
 
-	mainLabelsColumn += "<li><label for='name'>Project</label></li></ul></div></td>";
+	mainLabelsColumn += "</ul></div></td>";
 	otherLabelsColumn += "</ul></div></td>";
 	mainInputsColumn += "</ul></td>";
 	otherInputsColumn += "</ul></div></td>";
