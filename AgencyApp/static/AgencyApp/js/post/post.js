9caa4499776928d068b50bb5f79ea08fc89af9f5
matthew gray <mgray@rodeofx.com>
2017-08-13 12:02:20 -0700

    Change onclick of edit post button to shrink form if already open

--- AgencyApp/static/AgencyApp/js/post/post.js
+++ AgencyApp/static/AgencyApp/js/post/post.js
@@ -598,7 +598,7 @@ function createBrowseTableElement(elementDict, titleFieldName, elementType){
         elementString += "</ul>";
 
         // add edit button
-        elementString += "<div class='addNewPostButton' style='position:absolute; right: 0; bottom: 13%; text-align: center; width: 35px; height: 25px;' onclick='toggleExpandExistingForm(" + '"expand", "' + elementType + '", "' + elementDict["post"]["postID"]["value"] + '");' + "'><div style='font-size: 0.9em; font-weight: 500; margin-top: 1px;padding: 2px;'>Edit</div></div>";
+        elementString += "<div class='addNewPostButton' id='editPostButton_" + elementDict["post"]["postID"]["value"] + "' style='position:absolute; right: 0; bottom: 13%; text-align: center; width: 35px; height: 25px;' onclick='toggleExpandExistingForm(" + '"expand", "' + elementType + '", "' + elementDict["post"]["postID"]["value"] + '");' + "'><div style='font-size: 0.9em; font-weight: 500; margin-top: 1px;padding: 2px;'>Edit</div></div>";
 
         elementString += "</div></div></td>";
 
