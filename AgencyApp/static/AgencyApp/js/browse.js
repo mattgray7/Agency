a5802d5b0edfa23d6cbfcc24cb6971213be5069d
matthew gray <mgray@rodeofx.com>
2017-09-01 11:36:14 -0700

    Added framework to generate different search panel boxes

--- AgencyApp/static/AgencyApp/js/browse.js
+++ AgencyApp/static/AgencyApp/js/browse.js
@@ -1,3 +1,28 @@
-function helloWorld(){
-	console.log("helloWorld");
+function getSearchPanel(searchType){
+	/*if(searchType === "posts"){
+		return getPostsSearchPanel();
+	}else if(searchType === "users"){
+		return getUsersSearchPanel();
+	}else if(searchType === "events"){
+		return getEventsSearchPanel();
+	}*/
+	return getPostsSearchPanel();
+}
+
+function getPostsSearchPanel(){
+	panelString = "<div id='searchPanelContent' style='position: relative; height: 100%; width: 100%;'>";
+
+	// Add textbox
+	panelString += "<div class='browseSearchTextBoxContainer' style='position:absolute; top: 10px; left: 5%; right: 5%;'><div style='position: relative;'><div style='position: absolute; left: 0; right: 80px;'> <input type='text' placeholder='Browse projects, jobs, or roles'></div><div style='position: absolute; right: 0; padding: 4px; margin-top: 7px;' class='whiteButton blackHover'> Search</div></div>"
+
+	panelString += "</div>";
+	return panelString;
+}
+
+function getUsersSearchPanel(){
+	
+}
+
+function getEventsSearchPanel(){
+	
 }
