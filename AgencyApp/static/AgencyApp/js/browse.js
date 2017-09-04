c2c898fcc6fc5db64737ba47253ce3fdfa88dd29
matthew gray <mgray@rodeofx.com>
2017-09-04 15:13:39 -0700

    Convert tabs to spaces in browse.js

--- AgencyApp/static/AgencyApp/js/browse.js
+++ AgencyApp/static/AgencyApp/js/browse.js
@@ -3,97 +3,97 @@
 
 var browseTableElementHeight = 165;
 function createSearchResultsDisplay(resultList){
-	var displayString = '';
-	var tableHeight = 17;
-	for(section in resultList){
-		displayString += "<h1 style='position: relative; width: 100%; height: 40px;'><div style='position: absolute; top: 0; left: 5px;'>" + section + "</div></h1>";
-		tableHeight += 45
+    var displayString = '';
+    var tableHeight = 17;
+    for(section in resultList){
+        displayString += "<h1 style='position: relative; width: 100%; height: 40px;'><div style='position: absolute; top: 0; left: 5px;'>" + section + "</div></h1>";
+        tableHeight += 45
 
-		displayString += "<ul>"
-		for(var i=0; i < resultList[section].length; i++){
-			displayString += createBrowseListElement(section, resultList[section][i]);
-			tableHeight += browseTableElementHeight + 5;
-		}
-		displayString += "</ul>"
-	}
-	return {"html": displayString, "tableHeight": tableHeight}
+        displayString += "<ul>"
+        for(var i=0; i < resultList[section].length; i++){
+            displayString += createBrowseListElement(section, resultList[section][i]);
+            tableHeight += browseTableElementHeight + 5;
+        }
+        displayString += "</ul>"
+    }
+    return {"html": displayString, "tableHeight": tableHeight}
 }
 
 var createBrowseListElementFunctionMap = {"jobs": createJobElement, "roles": createRoleElement}
 function createBrowseListElement(elementType, dataDict){
-	var element = "<li style='height: " + browseTableElementHeight + "px; position: relative; overflow: hidden;'>"
-	
-	// Add picture
-	element += "<div style='position: absolute; left: 0; right: 75%; height: 100%; text-align: left;'><img src='" + dataDict["postPictureURL"] + "' style='margin-top: 8px; margin-left: 10px; max-height: 90%; max-width: 100%;'/></div>"
+    var element = "<li style='height: " + browseTableElementHeight + "px; position: relative; overflow: hidden;'>"
+    
+    // Add picture
+    element += "<div style='position: absolute; left: 0; right: 75%; height: 100%; text-align: left;'><img src='" + dataDict["postPictureURL"] + "' style='margin-top: 8px; margin-left: 10px; max-height: 90%; max-width: 100%;'/></div>"
 
-	if(elementType in createBrowseListElementFunctionMap){
-		element += createBrowseListElementFunctionMap[elementType](dataDict);
-	}else{
-		console.log("Could not find elementType " + elementType + " in function map")
-	}
-	element += "</li>";
-	return element;
+    if(elementType in createBrowseListElementFunctionMap){
+        element += createBrowseListElementFunctionMap[elementType](dataDict);
+    }else{
+        console.log("Could not find elementType " + elementType + " in function map")
+    }
+    element += "</li>";
+    return element;
 }
 
 function createRoleElement(dataDict){
-	var element = "<div style='position: absolute; left: 27%; right: 8px; top: 5px; height: 95%; text-align: left; color: rgba(0,0,0,0.7);'>"
+    var element = "<div style='position: absolute; left: 27%; right: 8px; top: 5px; height: 95%; text-align: left; color: rgba(0,0,0,0.7);'>"
 
-	// Add info content
-	element += "<h2 class='postInfoTitle'><a onclick='redirectToPost(" +'"' + dataDict["postID"] + '");' + "'>" + dataDict["title"] + "</a></h2>"
-	element += "<div style='color: rgba(0,0,0,0.5);'>" + dataDict["status"] + "</div>"
+    // Add info content
+    element += "<h2 class='postInfoTitle'><a onclick='redirectToPost(" +'"' + dataDict["postID"] + '");' + "'>" + dataDict["title"] + "</a></h2>"
+    element += "<div style='color: rgba(0,0,0,0.5);'>" + dataDict["status"] + "</div>"
 
-	// Add characterName
-	element += "<div>" + dataDict["characterName"] + "</div>";
+    // Add characterName
+    element += "<div>" + dataDict["characterName"] + "</div>";
 
-	// Add dates
-	element += getDateString(new Date(dataDict["startDate"]), new Date(dataDict["endDate"]));
+    // Add dates
+    element += getDateString(new Date(dataDict["startDate"]), new Date(dataDict["endDate"]));
 
-	// Add compensation
-	element += "<div style='color: rgba(0,0,0,0.7);'>"
-	if(dataDict["compensationType"] != null && dataDict["compensationType"].length > 0){
-		element += dataDict["compensationType"]
-		if(dataDict["compensationDescription"] != null){
-			element += " - " + dataDict["compensationDescription"]
-		}
-	}else{
-		element += "Compensation unspecified";
-	}
-	element += "</div>"
+    // Add compensation
+    element += "<div style='color: rgba(0,0,0,0.7);'>"
+    if(dataDict["compensationType"] != null && dataDict["compensationType"].length > 0){
+        element += dataDict["compensationType"]
+        if(dataDict["compensationDescription"] != null){
+            element += " - " + dataDict["compensationDescription"]
+        }
+    }else{
+        element += "Compensation unspecified";
+    }
+    element += "</div>"
 
-	// Add description
-	element += "<div style='height: 50px; position: absolute: right: 0; text-align: left;'>" + dataDict["description"] + "</div>"
+    // Add description
+    element += "<div style='height: 50px; position: absolute: right: 0; text-align: left;'>" + dataDict["description"] + "</div>"
 
-	element += "</div>";
-	return element
+    element += "</div>";
+    return element
 }
 
 function createJobElement(dataDict){
-	var element = "<div style='position: absolute; left: 27%; right: 8px; top: 5px; height: 95%; text-align: left; color: rgba(0,0,0,0.7);'>"
+    var element = "<div style='position: absolute; left: 27%; right: 8px; top: 5px; height: 95%; text-align: left; color: rgba(0,0,0,0.7);'>"
 
-	// Add info content
-	element += "<h2 class='postInfoTitle'><a onclick='redirectToPost(" +'"' + dataDict["postID"] + '");' + "'>" + dataDict["title"] + "</a></h2>"
-	element += "<div style='color: rgba(0,0,0,0.5);'>" + dataDict["status"] + "</div>"
+    // Add info content
+    element += "<h2 class='postInfoTitle'><a onclick='redirectToPost(" +'"' + dataDict["postID"] + '");' + "'>" + dataDict["title"] + "</a></h2>"
+    element += "<div style='color: rgba(0,0,0,0.5);'>" + dataDict["status"] + "</div>"
 
-	// Add dates
-	element += getDateString(new Date(dataDict["startDate"]), new Date(dataDict["endDate"]));
+    // Add dates
+    element += getDateString(new Date(dataDict["startDate"]), new Date(dataDict["endDate"]));
 
-	// Add compensation
-	element += "<div style='color: rgba(0,0,0,0.7);'>"
-	if(dataDict["compensationType"] != null && dataDict["compensationType"].length > 0){
-		element += dataDict["compensationType"]
-		if(dataDict["compensationDescription"] != null){
-			element += " - " + dataDict["compensationDescription"]
-		}
-	}else{
-		element += "Compensation unspecified";
-	}
-	element += "</div>"
+    // Add compensation
+    element += "<div style='color: rgba(0,0,0,0.7);'>"
+    if(dataDict["compensationType"] != null && dataDict["compensationType"].length > 0){
+        element += dataDict["compensationType"]
+        if(dataDict["compensationDescription"] != null){
+            element += " - " + dataDict["compensationDescription"]
+        }
+    }else{
+        element += "Compensation unspecified";
+    }
+    element += "</div>"
 
-	// Add description
-	element += "<div style='height: 50px; position: absolute: right: 0; text-align: left;'>" + dataDict["description"] + "</div>"
+    // Add description
+    element += "<div style='height: 50px; position: absolute: right: 0; text-align: left;'>" + dataDict["description"] + "</div>"
 
-	element += "</div>";
-	return element
+    element += "</div>";
+    return element
 }
 
 function addBrowseDropdownCallback(callbackFunctionName, secondaryEnterSubmitButton, extraInputs){
@@ -126,7 +126,7 @@ function addBrowseDropdownCallback(callbackFunctionName, secondaryEnterSubmitBut
 }
 
 function selectBrowseSuggestion(value, textDivName, dropdownDivName){
-	var textDiv = document.getElementById(textDivName);
+    var textDiv = document.getElementById(textDivName);
     var dropdownDiv = document.getElementById(dropdownDivName);
     if(textDiv != null){
         textDiv.value = value;
@@ -150,14 +150,14 @@ function previewTextInDropdown(textInputDivName, dropdownDivName, getDataFunctio
 function getPreviewBrowseSuggestionsString(suggestions){
     var previewString = "<ul id='browseSuggestionDropdownList' style='margin-bottom: -20px;'>";
     if(suggestions != null){
-    	for(type in suggestions){
-    		for(var i=0; i < suggestions[type].length; i++){
-    			previewString += "<li style='border: none;' onclick='selectBrowseSuggestion(" + '"' + suggestions[type][i] + '", "searchTextInput", "browseDropdown");' + "'><div style='position:relative; height: 30px; text-align: left; margin-left: 4px;'>" + suggestions[type][i] + " - " + type + "</div></li>";
-    		}
-    	}
-	}else{
-		previewString += "No suggestions"
-	}
+        for(type in suggestions){
+            for(var i=0; i < suggestions[type].length; i++){
+                previewString += "<li style='border: none;' onclick='selectBrowseSuggestion(" + '"' + suggestions[type][i] + '", "searchTextInput", "browseDropdown");' + "'><div style='position:relative; height: 30px; text-align: left; margin-left: 4px;'>" + suggestions[type][i] + " - " + type + "</div></li>";
+            }
+        }
+    }else{
+        previewString += "No suggestions"
+    }
     previewString += "</ul>";
     return previewString;
 }
@@ -170,7 +170,7 @@ function searchPreviewBrowseSuggestions(textValue, container, extraInputs){
         if(textValue.length != 0){
             container.style.display = "block";
             var suggestions;
-			$.ajax({
+            $.ajax({
                 url : "/ajax/getSearchSuggestions/",
                 data : {"text": textValue},
                 type : 'POST',
@@ -178,13 +178,13 @@ function searchPreviewBrowseSuggestions(textValue, container, extraInputs){
                 success : function(data) {
                     if(data["success"]){
                         if(data["suggestions"] != null){
-                        	suggestions = data["suggestions"];
+                            suggestions = data["suggestions"];
                         }
                     }else{
                         container.innerHTML = "No user found with name " + textValue
                     }
                     var suggestionsString = getPreviewBrowseSuggestionsString(suggestions)
-	    			container.innerHTML = suggestionsString;
+                    container.innerHTML = suggestionsString;
                 }
             });
         }else{
